efae8e1acfb2e932b3e5886a8e762568b1ec2fb7
Vladislav <vlad-pugovkin2@tandex.ru>
2023-06-23 18:46:58 +0300

    Add HW_2

---
+++ HW2/HW2/src/main/java/org/example/ListOne.java
@@ -0,0 +1,44 @@
+package org.example;
+
+public class ListOne {
+    Node head;
+    //List.Node tail;
+
+
+    public void push(int value){
+        Node node = new Node();
+        node.value = value;
+        node.next = head;
+        head = node;
+    }
+
+    public Integer pop(){
+        Integer result = null;
+        if (head != null){
+            result= head.value;
+            head = head.next;
+        }
+        return result;
+    }
+
+    public void revert(){
+        if(head != null && head.next != null){
+            revert(head.next, head);
+        }
+    }
+    private void revert(Node currentNode, Node previousNode){
+        if(currentNode.next == null){
+            head = currentNode;
+        }else {
+            revert(currentNode.next, currentNode);
+        }
+        currentNode.next = previousNode;
+        previousNode.next = null;
+    }
+
+    public class Node{
+        int value;
+        Node next;
+        //List.Node previous;
+    }
+}
